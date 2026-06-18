9fd4b0a92c1ccfffdb08dff5b34cede35e5f1b8d
Abhinav Kumar <kumarabhinav1908@gmail.com>
2026-06-18 10:44:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+//Global Scope
+// var age = 15;
+
+// console.log(age);
+
+// {
+//     console.log(age)
+// }
+// if(true){
+//     console.log(age)
+// }
+// for(let i =0; i<2; i++) {
+//     console.log(age);
+// }
+
+// function sayHello() {
+//     console.log("hi ", age);
+// }
+// sayHello();
+
+// function sayHello() {
+//     var fullName = "Earth";
+//     console.log("Hello Dunia ", name);
+// }
+// console.log(fullName);
+
+// sayHello();
+// console.log(height);
+// {
+//     var height = 180;
+// }
+
+// console.log(height);
+
+// {
+//     const age = 100;
+// }
+
+// console.log(age);
+console.log(marks);
+console.log("Abhinav");
+console.log("Kumar");
+const marks = 100;
+console.log(marks);
